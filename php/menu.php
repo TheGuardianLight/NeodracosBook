5f17a315b10468c6b279b7b23f1c22c8f7b9aa20
Noa Ledet <noa.ledet@proton.me>
2024-08-08 16:19:08 +0200

    Modification du menu

--- php/menu.php
+++ php/menu.php
@@ -7,7 +7,7 @@
         <div class="collapse navbar-collapse" id="navbarNav">
             <ul class="navbar-nav">
                 <li class="nav-item">
-                    <a class="nav-link active" aria-current="page" href="#">Accueil</a>
+                    <a class="nav-link active" aria-current="page" href="index.php">Accueil</a>
                 </li>
                 <li class="nav-item">
                     <a class="nav-link disabled" href="#">Livres</a>
